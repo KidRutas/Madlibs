45ee792352b82670f529eb6783421c46608cf8af
KidRutas <45852468+KidRutas@users.noreply.github.com>
2024-07-10 15:16:52 +0200

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -5,4 +5,4 @@ youtuber = "" # some string variable
 # a few ways to do this 
 print("subscribe to " + youtuber)
 print("subscribe to {}".format(youtuber))
-print(f"subscribe to 
+print(f"subscribe to {youtuber}")
